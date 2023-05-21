262e8a639332f5f2d49f81173aa695cf7a0f14fa
Rupesh Dangi <83266954+rupysdxe@users.noreply.github.com>
2023-05-21 13:36:54 +0545

    remove: remove user mapper

--- src/main/java/com/rupesh/jwtauthentication/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.rupesh.jwtauthentication.mapper;
-
-import com.rupesh.jwtauthentication.auth.dto.UserDto;
-import com.rupesh.jwtauthentication.auth.entity.User;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public  interface UserMapper extends EntityMapper<User, UserDto>{
-}
